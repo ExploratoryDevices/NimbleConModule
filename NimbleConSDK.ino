8a47cae7046eade2d8a89d5af74b8af3555c7eb0
Exploratory Devices <72473317+ExploratoryDevices@users.noreply.github.com>
2022-12-26 16:37:20 -0800

    Update NimbleConSDK.ino

--- NimbleConSDK.ino
+++ NimbleConSDK.ino
@@ -18,10 +18,12 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+// ***************** Do stuff to the values to be sent below this line. Use no delays.
+  
   // Check actuator and pendant serial ports for complete packets and update structs.
   readFromPend();
   if(readFromPend())  // Read values from pendant. If the function returns true, the values were updated so update the pass-through values.
-  {
+  { // DEMO: Pass through data from pendant to actuator
     actuator.positionCommand = pendant.positionCommand;
     actuator.forceCommand = pendant.forceCommand;
     actuator.airIn = pendant.airIn;
@@ -31,15 +33,14 @@ void loop() {
   
   readFromAct(); // Read values from actuator. If the function returns true, the values were updated. Otherwise there was nothing new.
 
-// ***************** Do stuff to the values to be sent below this line. Use no delays.
-
+  // This DEMO code pauses the actuator (in a very crude way) when the encoder button is pressed (it will jump to whatever position the pendant is commanding at the moment the button is released)
   if(digitalRead(ENC_BUTT)) // Encoder button reads low when pressed.
   {
-    driveLEDs(encoder.getCount());
+    driveLEDs(encoder.getCount());  // Show LEDs as demo
   }else
   {
-    driveLEDs(0);
-    actuator.forceCommand = 0;
+    driveLEDs(0);   // Blank LEDs when button is pressed
+    actuator.forceCommand = 0;  // Set force command to 0 when button is pressed.
   }
 
 // ***************** Do stuff to the values to be sent above this line. Use no delays.
